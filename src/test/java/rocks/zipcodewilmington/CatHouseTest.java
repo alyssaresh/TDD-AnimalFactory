e60d2e9ba9da16a4ee64801b7ed5f269a25a756b
Alyssa Resh <aresh@zipcoders-mbp.mynetworksettings.com>
2024-11-17 22:30:29 -0500

    still working on cathouse tests

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,10 +1,40 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
+import static rocks.zipcodewilmington.animals.animal_storage.CatHouse.add;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
-    // TODO - Create tests for `void add(Cat cat)`
+
+    Cat puck = new Cat("Puck", new Date(), 20);
+
+    @Test
+    public void addCatTest() {
+        CatHouse.add(puck);
+
+        Integer expectedSize = 1;
+        Integer actualSize = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void removeCatTest(){
+        CatHouse.remove(puck);
+        Integer expectedSize = 0;
+        Integer actualSize = CatHouse.getNumberOfCats();
+        Assert.assertEquals(expectedSize, actualSize);
+    }
+
+
     // TODO - Create tests for `void remove(Integer id)`
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
